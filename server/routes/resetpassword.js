efc137ba667d13e3f6f7dd996e281a8ffbed95e7
yash2870 <94793349+yash2870@users.noreply.github.com>
2025-01-23 11:36:01 +0530

    Update resetpassword.js

--- server/routes/resetpassword.js
+++ server/routes/resetpassword.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const crypto = require('crypto');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const User = require('../models/User'); // Ensure the User model path is correct
 
 const router = express.Router();
