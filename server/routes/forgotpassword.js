48a5f3fcd41595c1dd998f48fc48dbf70c1a281d
yash2870 <94793349+yash2870@users.noreply.github.com>
2025-01-23 11:36:18 +0530

    Update forgotpassword.js

--- server/routes/forgotpassword.js
+++ server/routes/forgotpassword.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const crypto = require('crypto');
 const nodemailer = require('nodemailer');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const User = require('../models/User'); 
 
 const router = express.Router();
